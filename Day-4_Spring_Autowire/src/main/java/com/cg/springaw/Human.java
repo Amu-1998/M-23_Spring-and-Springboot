92bbbf146d5f4f88b7b5ef1f6a452ae53a92975e
Solv-IT <Solv-IT@DESKTOP-5TK16MH>
2022-09-22 15:07:07 +0530

    First commit

---
+++ Day-4_Spring_Autowire/src/main/java/com/cg/springaw/Human.java
@@ -0,0 +1,32 @@
+package com.cg.springaw;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Human {
+	//dependency in term of object
+	private Heart heart;
+	
+	//achieving DI using setters
+	@Autowired
+	@Qualifier("Humanheart")
+	public void setHeart(Heart heart) {
+		this.heart = heart;
+	}
+	
+	//using constructor in beans instead of byName write constructor
+	public Human(Heart heart) {
+		super();
+		this.heart = heart;
+	}
+	
+	public Human() {
+		this.heart=heart;
+	}
+
+	public void pumping()
+	{
+		heart.pump();
+		System.out.println("Name of the animal is: "+heart.getNameOfAnimal()+" "+"No of heart is: "+heart.getNoOfHeart());
+	}
+}
